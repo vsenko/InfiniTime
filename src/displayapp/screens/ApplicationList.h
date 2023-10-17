fdefddc6b5c39ea29d2a5388f5f3260faada0046
vsenko <vsenko@gmail.com>
2023-10-17 15:07:09 +0000

    Remove games

--- src/displayapp/screens/ApplicationList.h
+++ src/displayapp/screens/ApplicationList.h
@@ -37,7 +37,7 @@ namespace Pinetime {
         static constexpr int appsPerScreen = 6;
 
         // Increment this when more space is needed
-        static constexpr int nScreens = 2;
+        static constexpr int nScreens = 1;
 
         static constexpr std::array<Tile::Applications, appsPerScreen * nScreens> applications {{
           {Symbols::stopWatch, Apps::StopWatch},
@@ -45,14 +45,14 @@ namespace Pinetime {
           {Symbols::hourGlass, Apps::Timer},
           {Symbols::shoe, Apps::Steps},
           {Symbols::heartBeat, Apps::HeartRate},
-          {Symbols::music, Apps::Music},
+          // {Symbols::music, Apps::Music},
 
-          {Symbols::paintbrush, Apps::Paint},
-          {Symbols::paddle, Apps::Paddle},
-          {"2", Apps::Twos},
+          // {Symbols::paintbrush, Apps::Paint},
+          // {Symbols::paddle, Apps::Paddle},
+          // {"2", Apps::Twos},
           {Symbols::drum, Apps::Metronome},
-          {Symbols::map, Apps::Navigation},
-          {Symbols::none, Apps::None},
+          // {Symbols::map, Apps::Navigation},
+          // {Symbols::none, Apps::None},
 
           // {"M", Apps::Motion},
         }};
